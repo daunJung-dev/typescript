9c09fd40569dc23a7197b93f55227ccbece96283
정다운 <dkun9039@paycrux.co.kr>
2021-01-21 23:40:49 +0900

    init: 0. Hello Typescript!

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log('Hello world~!');
+console.log("this is watching mode");
+var Car = /** @class */ (function () {
+    function Car() {
+        this.engine = 1;
+    }
+    return Car;
+}());
---
+++ main.ts
@@ -0,0 +1,8 @@
+console.log('Hello world~!');
+console.log("this is watching mode");
+class Car {
+    engine:number;
+    constructor() {
+        this.engine =1;
+    }
+}
