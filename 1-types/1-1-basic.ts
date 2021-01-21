76bb60d6f9da30786bbeb5b4196ad4f3700a7b93
정다운 <dkun9039@paycrux.co.kr>
2021-01-21 23:53:06 +0900

    feat: basic 변수

---
+++ 1-types/1-1-basic.ts
@@ -0,0 +1,15 @@
+{
+    // Javascript에 대한 이해가 있다는 전제로 진행됩니다. :)
+    // old: var 💩
+    var age = 5;
+    age = 1;
+
+    // let es6
+    let name = "hello";
+    name = 'hi';
+
+    // const
+    const age = 5;
+    age = 3;
+
+}
