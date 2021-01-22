bd9d067f4793eb4722e96fe463e26e90d332fdc9
정다운 <dkun9039@paycrux.co.kr>
2021-01-22 22:28:27 +0900

    feat: 1-4 aliases Type

---
+++ 1-types/1-4-aliases.ts
@@ -0,0 +1,28 @@
+{
+    /**
+     * Type Aliases
+     */
+    type Text = string;
+    const name: Text = 'ellie';
+    const address: Text = 'korea';
+    type Num = number;
+    type Student = {
+        name: Text;
+        age: Num;
+    }
+    const student: Student = {
+        age: 12,
+        name: 'daun'
+    }
+
+    /**
+     * String Literal Types
+     */
+    type Name = 'name';
+    let ellieName: Name;
+    ellieName = 'name'; // 
+    type JSON = 'json';
+    const json:JSON ="json";
+
+    
+}
