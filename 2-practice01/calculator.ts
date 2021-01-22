2998ace0d4c01c4628b1bfed2b5d0a40b380a7d1
정다운 <dkun9039@paycrux.co.kr>
2021-01-22 23:42:38 +0900

    fix: 계산기 함수 수정

--- 2-practice01/calculator.ts
+++ 2-practice01/calculator.ts
@@ -3,15 +3,29 @@
  */
 type CalculateType = 'add'| 'subtract' | 'multiply' | 'divide' | 'remainder';
 
-function calculate(type: CalculateType, firstNum: number, secondNum: number):number {
-    if( type === 'add') return firstNum + secondNum
-    else if( type === 'subtract') return firstNum - secondNum
-    else if ( type === 'multiply') return firstNum * secondNum
-    else if ( type === 'divide') return firstNum / secondNum
-    else if ( type === 'remainder') return firstNum % secondNum
+function calculate(type: CalculateType, a: number, b: number):number {
+    switch(type) {
+        case 'add':
+            return a + b;
+        case "subtract":
+            return a - b;
+        case 'multiply':
+            return a * b;
+        case "divide":
+            return a / b;
+        case "remainder":
+            return a % b;
+        default:
+            throw Error('unknown command');
+    }
+    // if( type === 'add') return a + b
+    // else if( type === 'subtract') return a - b
+    // else if ( type === 'multiply') return a * b
+    // else if ( type === 'divide') return a / b
+    // else if ( type === 'remainder') return a % b
 }
- console.log(calculate('add', 1,3)); // 4
- console.log(calculate('subtract', 3,1))// 2
- console.log(calculate('multiply', 4, 2))// 8
- console.log(calculate('divide', 4,2))// 2
- console.log(calculate('remainder', 5, 2))// 1
+console.log(calculate('add', 1,3)); // 4
+console.log(calculate('subtract', 3,1))// 2
+console.log(calculate('multiply', 4, 2))// 8
+console.log(calculate('divide', 4,2))// 2
+console.log(calculate('remainder', 5, 2))// 1
