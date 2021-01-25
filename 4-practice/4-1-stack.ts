371be41293bbcc69b7fd53648d073694e38d4f16
정다운 <dkun9039@paycrux.co.kr>
2021-01-25 22:02:30 +0900

    feat: 4-1 solve practice - 배열을 이용하지 않고 스택 구현하기

--- 4-practice/4-1-stack.ts
+++ 4-practice/4-1-stack.ts
@@ -4,30 +4,46 @@
     push(value: string): void;
     pop(): string;
   }
+
+  type StackNode = {
+    readonly value: string;
+    readonly next?: StackNode;
+  };
+
   class StackImpl implements Stack {
-    private stackArr: string[];
-
+    private _size: number = 0;
+    private head?: StackNode;
     get size(): number {
-      return this.stackArr.length;
+      return this._size;
     }
 
-    constructor(...args: string[]) {
-      this.stackArr = args;
-    }
+    constructor(private capacity?: number) {}
 
-    push(a: string): void {
-      this.stackArr[this.stackArr.length] = a;
+    push(value: string): void {
+      if (this.size === this.capacity) {
+        throw new Error("Stack is full🌝");
+      }
+      this._size++;
+      const node: StackNode = { value, next: this.head };
+      this.head = node;
     }
     pop(): string {
-      const lastOne = this.stackArr[this.stackArr.length - 1];
-      this.stackArr = this.stackArr.slice(0, this.stackArr.length - 1);
-      return lastOne;
+      if (this.head == null) {
+        throw new Error("Stack is empty! ✋");
+      }
+      const node = this.head;
+      this.head = node.next;
+      this._size--;
+      return node.value;
     }
   }
 
-  const test = new StackImpl("ellie");
+  const test = new StackImpl();
   test.push("Bill");
+  test.push("ellie");
   console.log(test, test.size);
   console.log(test.pop());
+  console.log(test.pop());
   console.log(test, test.size);
+  console.log(test.pop());
 }
