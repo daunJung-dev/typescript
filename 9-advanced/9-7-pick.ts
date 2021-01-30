b95b8212cc1922caabc782ceefc8db95137ec138
정다운 <dkun9039@paycrux.co.kr>
2021-01-31 00:40:50 +0900

    feat: Pick 타입

---
+++ 9-advanced/9-7-pick.ts
@@ -0,0 +1,26 @@
+{
+  type Video = {
+    id: string;
+    title: string;
+    url: string;
+    data: string;
+  };
+
+  type VideoMetadata = Pick<Video, "id" | "title">;
+
+  function getVideo(id: string): Video {
+    return {
+      id,
+      title: "video",
+      url: "https://...",
+      data: "byte-data..",
+    };
+  }
+
+  function getVideoMetadata(id: string): VideoMetadata {
+    return {
+      id: id,
+      title: "title",
+    };
+  }
+}
