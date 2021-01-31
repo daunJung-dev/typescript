a3dbdbe9c2174bdf89a4c60522c2652dc313acf4
정다운 <dkun9039@paycrux.co.kr>
2021-01-31 11:43:03 +0900

    feat: Record

--- 9-advanced/9-9-record.ts
+++ 9-advanced/9-9-record.ts
@@ -9,3 +9,6 @@ const nav: Record<Page, PageInfo> = {
   about: { title: "About" },
   contact: { title: "Contact" },
 };
+
+type Product = "cat" | "dog";
+type NewProduct = Capitalize<Product>; // CAT DOG
